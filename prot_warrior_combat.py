8d723dce70e0aea377e82e7414aea94310cab145
KrakenVVagon <73725558+KrakenVVagon@users.noreply.github.com>
2021-01-27 00:52:13 -0500

    Update prot_warrior_combat.py

--- prot_warrior_combat.py
+++ prot_warrior_combat.py
@@ -4,5 +4,12 @@ Created on Mon Dec  7 22:52:45 2020
 
 @author: Andrew
 
-Combat sim! Starting with warrior
+Combat sim!
+
+How will this work?
+
+1. Set a combat duration. This will be the core "while" loop that will tick down in counts of GCD seconds
+2. Every loop we need to deal damage (based on ability priority)
+3. Every loop we need to update our stats for procs and damage
+4. Every loop we also need to increment ability CDs and buff durations
 """
